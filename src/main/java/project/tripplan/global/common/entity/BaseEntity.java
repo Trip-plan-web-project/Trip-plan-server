5343170728b87006b91d1b73613876e4d5a08864
KANG JISEOK <rkdwltjr5876@naver.com>
2025-01-05 16:40:22 +0900

    [chore] BaseEntity 카멜케이스로 변경

--- src/main/java/project/tripplan/global/common/entity/BaseEntity.java
+++ src/main/java/project/tripplan/global/common/entity/BaseEntity.java
@@ -17,8 +17,8 @@ public abstract class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
-    private LocalDateTime created_at;
+    private LocalDateTime CreatedAt;
 
     @LastModifiedDate
-    private LocalDateTime updated_at;
+    private LocalDateTime UpdatedAt;
 }
