3e0b2516324d1f8de94e5269c0ca6d1885af8989
KANG JISEOK <rkdwltjr5876@naver.com>
2025-01-21 00:45:33 +0900

    [chore] BaseResponseCode (USER_GET_SUCCESS) 수정

--- src/main/java/project/tripplan/global/common/response/BaseResponseCode.java
+++ src/main/java/project/tripplan/global/common/response/BaseResponseCode.java
@@ -13,6 +13,9 @@ public enum BaseResponseCode {
 	 * 1000 : 성공
 	 */
 
+	//user 1000~
+	USER_GET_SUCCESS(true, 1001, "회원 정보 가져오기에 성공했습니다.", HttpStatus.OK),
+
 	//comment 1200 ~
 	ADD_COMMENT_SUCCESS(true, 1201, "댓글을 추가하였습니다.", HttpStatus.OK),
 	DELETE_COMMENT_SUCCESS(true, 1202, "댓글을 삭제하였습니다.", HttpStatus.OK),
@@ -39,7 +42,6 @@ public enum BaseResponseCode {
 	USER_NOT_EXIST(false, 2001, "존재하지 않는 회원입니다.", HttpStatus.NOT_FOUND),
 	REFRESHTOKEN_NOT_EXIST(false, 2002, "리프레시 토큰이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
 	INVALID_OAUTH2_REQUEST(false, 2003, "유효하지 않은 OAuth2 요청입니다.", HttpStatus.BAD_REQUEST),
-	USER_GET_SUCCESS(false, 2004, "회원 정보 가져오기에 성공했습니다.", HttpStatus.BAD_REQUEST),
 
 	//plan 2100 ~,
 	PLAN_NOT_EXIST(false, 2101, "존재하지 않는 게시글입니다.", HttpStatus.NOT_FOUND),
