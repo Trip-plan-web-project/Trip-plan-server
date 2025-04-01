2fde6e8efd358d35881c052b318b57f64d80c540
KANG JISEOK <rkdwltjr5876@naver.com>
2025-04-01 18:17:21 +0900

    [fix] plan search 비공개 게시글 조회되는 버그 수정

--- src/main/java/project/tripplan/domain/plan/repository/PlanRepositoryCustomImpl.java
+++ src/main/java/project/tripplan/domain/plan/repository/PlanRepositoryCustomImpl.java
@@ -75,6 +75,7 @@ public class PlanRepositoryCustomImpl implements PlanRepositoryCustom {
 
 		// 필터링 조건
 		BooleanBuilder builder = buildSearchCondition(req, ppcFilter, pcFilter);
+		builder.and(plan.status.eq(PlanStatus.PUBLIC));
 
 		// 쿼리 생성
 		JPAQuery<Plan> query = qf
@@ -228,6 +229,8 @@ public class PlanRepositoryCustomImpl implements PlanRepositoryCustom {
 		QPlaceCategory pcFilter = new QPlaceCategory("pcFilter");
 
 		BooleanBuilder builder = buildSearchCondition(req, ppcFilter, pcFilter);
+
+		builder.and(plan.status.eq(PlanStatus.PUBLIC));
 
 		Long countResult = qf
 			.select(plan.countDistinct())
